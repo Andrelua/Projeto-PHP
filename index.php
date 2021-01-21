9f60b55b50aac24bd1f88492f9b4cc19ad5fd69f
André Luan <Andreluan176@gmail.com>
2021-01-21 15:24:45 -0300

    Add

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <!--Lembrar de criar a tabela no banco de dados produtos e criar uma conexão
-        com o pedido-->
+    <!--Lembrar de criar a tabela no banco de dados 'produto' e criar uma conexão
+        com o pedido. Com os campos: id_produto, nome_produto, preco_produto, qtd_produto e id_pedido.-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
