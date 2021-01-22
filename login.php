1ee716a239b69e943f9735108e876d9afae81ba9
André Luan <Andreluan176@gmail.com>
2021-01-22 18:36:46 -0300

    Salvando o id do pedido

--- login.php
+++ login.php
@@ -33,7 +33,9 @@ if (isset($_POST["btn-login"])):
                 if(mysqli_num_rows($resultado) == 0):
                     $sql = "INSERT INTO pedidos (numero_pedido, id_cliente) VALUES ('$id', '$id_usuario')";
                     $resultado1 = mysqli_query($connect, $sql);
-                    $dados_pedidos = mysqli_fetch_array($resultado);
+                    $consulta = "SELECT * FROM pedidos WHERE numero_pedido = '$id' AND id_cliente = $id_usuario";
+                    $result_1 = mysqli_query($connect, $consulta);
+                    $dados_pedidos = mysqli_fetch_array($result_1);
                     if($resultado1):
                         $_SESSION['id_pedido'] = $dados_pedidos['id_pedido'];
                         header('Location: index.php');
@@ -42,7 +44,9 @@ if (isset($_POST["btn-login"])):
                     $id += 1;
                     $sql = "INSERT INTO pedidos (numero_pedido, id_cliente) VALUES ('$id', '$id_usuario')";
                     $resultado2 = mysqli_query($connect, $sql);
-                    $dados_pedidos = mysqli_fetch_array($resultado);
+                    $consulta = "SELECT * FROM pedidos WHERE numero_pedido = '$id' AND id_cliente = $id_usuario";
+                    $result_2 = mysqli_query($connect, $consulta);
+                    $dados_pedidos = mysqli_fetch_array($result_2);
                     if($resultado2):
                         $_SESSION['id_pedido'] = $dados_pedidos['id_pedido'];
                         header('Location: index.php');
